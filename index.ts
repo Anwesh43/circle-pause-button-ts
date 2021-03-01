ffe4b21e9609da06690681d67b57a4ec8674dfba
Anwesh43 <anweshthecool0@gmail.com>
2021-03-01 21:44:08 +0530

    adding CPBNode class

--- index.ts
+++ index.ts
@@ -149,3 +149,45 @@ class Animator {
         }
     }
 }
+
+class CPBNode {
+
+    prev : CPBNode 
+    next : CPBNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new CPBNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawCPBNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : CPBNode {
+        var curr : CPBNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr != null) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
